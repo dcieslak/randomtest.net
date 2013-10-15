ef6dcd56a7c0c83ee07a7d0e790f8a7c49951d44
Dariusz Cieslak <dc@aplikacja.info>
2013-10-15 23:21:26 +0200

    Basic server with error counting and a report (#3)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: build test
+all: build src/server/var test
 
 build:
 	make -C src/probes/c
@@ -6,6 +6,12 @@ build:
 test:
 	make -C src/probes/c test
 	make -C src/probes/php test
+	wget -qO - $$RANDOMTEST_URL
+
+src/server/var:
+	mkdir src/server/var
+	# PHP process (typically www-data) acess granted
+	chmod o+w src/server/var
 
 clean:
 	make -C src/probes/c clean
--- src/server/randomtest-server.php
+++ src/server/randomtest-server.php
@@ -1,10 +1,37 @@
 <?php
 
-header('Content-type: text/plain');
+$DATABASE_FILE = "var/randomtest.db";
+$db = dba_open($DATABASE_FILE, "c");
 
-echo "OK\n";
-echo $_POST['stacktrace'];
+if (isset($_POST['stacktrace'])) {
+    header('Content-type: text/plain');
 
-file_put_contents('/tmp/randomtest.log', $_POST['stacktrace']);
+    echo "OK\n";
+    $stacktrace = $_POST['stacktrace'];
+    echo $stacktrace;
+
+    if (dba_exists($stacktrace, $db)) {
+        $counter = dba_fetch($stacktrace, $db) + 1;
+    }
+    else {
+        $counter = 1;
+    }
+    dba_replace($stacktrace, $counter, $db);
+}
+else {
+    header('Content-type: text/plain');
+    echo "RandomTest.net report\n\n";
+    for($stacktrace = dba_firstkey($db); $stacktrace != false; $stacktrace = dba_nextkey($db)) {
+        $counter = dba_fetch($stacktrace, $db);
+        $arr[$stacktrace] = $counter;
+    }
+    // sort array by counter, keys=stacktraces left
+    arsort($arr);
+    foreach ($arr as $stacktrace => $counter) {
+        echo "event counter: $counter\n$stacktrace\n";
+    }
+}
+
+dba_close($db);
 
 ?>
